8a1d32ffe9bd82de417b78a8721dfb7e48511ff4
Emre <emre.ertrk.3455@gmail.com>
2023-11-29 18:09:07 +0300

    tasks completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import entity.Employee;
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Employee> employees = new LinkedList<>();
+        employees.add(new Employee(1, "emrah","kızıltan"));
+        employees.add(new Employee(2, "ali","kızıltan"));
+        employees.add(new Employee(2, "emra","kızıltan"));
+        employees.add(new Employee(2, "emre","kızıltan"));
+        employees.add(new Employee(3, "ahmet","kızıltan"));
+        employees.add(new Employee(3, "can","kızıltan"));
+        employees.add(new Employee(4, "hazal","kızıltan"));
+        employees.add(new Employee(4, "sertaç","kızıltan"));
+
+
+        Map<Integer, Employee> employeeMap = new HashMap<>();
+        List<Employee> removedEmployees= new ArrayList<>();
+
+        Iterator<Employee> iterator =  employees.iterator();
+        while (iterator.hasNext()){
+            Employee employee = iterator.next();
+            if(employee==null){
+                System.out.println("null data bulundu");
+                continue;
+            }
+            if(employeeMap.containsKey(employee.getId())){
+                removedEmployees.add(employee);
+                iterator.remove();
+            }else{
+                employeeMap.put(employee.getId(),employee);
+            }
+        }
+        System.out.println("mevcut:");
+        System.out.println(employees);
+        System.out.println("tekiller:");
+        System.out.println(employeeMap);
+        System.out.println("silinenler: ");
+        System.out.println(removedEmployees);
     }
 }
---
+++ src/entity/Employee.java
@@ -0,0 +1,47 @@
+package entity;
+
+public class Employee {
+    private String firstname;
+    private String lastname;
+    private int id;
+
+    public Employee(int id, String firstname, String lastname ) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
